6b7a6bde311933ab1336cb470001d4d3bd718337
garethmcmillan <gareth.mcmillan@gmail.com>
2017-02-02 08:57:31 +0000

    pulling out the variables

--- manifests/12c/client.pp
+++ manifests/12c/client.pp
@@ -2,26 +2,19 @@
 #
 class oracle::12c::client() {
 
-  $oracle_home    ='/u01/app/oracle/product/12.1/client'
-  $oracle_base    ='/u01/app/oracle'
-  $installer      ='/home/oracle/sig/client'
-  $media_home     ='/opt/media'
-  $dbhostname     ='ol7.localdomain'
-  $dbport         ='1521'
-  $servicename    ='orcl.localdomain'
-  $sid            ='orcl'
-
-#  file { '/home/oracle/.bashrc':
-#      owner => 'oracle',
-#      group => 'dba',
-#      mode  => '0644',
-#      content => template('oracle/bashrc.sh.erb'),
-#  }
+  $oracle_home
+  $oracle_base
+  $installer_location
+  $source_location
+  $dbhostname
+  $dbport
+  $servicename
+  $sid
 
 # template(<FILE REFERENCE>, [<ADDITIONAL FILES>, ...])
  file { 'client-response':
    ensure    => file,
-   path      => '/home/oracle/sig/client/client_install.rsp',
+   path      => "${installer_location}/client_install.rsp",
    owner     => oracle,
    group     => oinstall,
    content   => template('oracle/client_install.rsp.erb'),
@@ -36,16 +29,16 @@ class oracle::12c::client() {
  }
 
   exec { "unzipClient":
-    command   => "/usr/bin/unzip ${media_home}/3rdParty/oracle/linuxamd64_12102_client.zip",
+    command   => "/usr/bin/unzip ${source_location}/3rdParty/oracle/linuxamd64_12102_client.zip",
     cwd       => $installer,
-    creates   => "${installer}/client/runInstaller",
+    creates   => "${installer_location}/client/runInstaller",
     user      => oracle,
     group     => dba,
     logoutput => true,
   }
 
   exec { "clientInstall":
-    command     => "${installer}/client/runInstaller -silent -waitforcompletion -ignoreSysPrereqs -ignorePrereq -responseFile /home/oracle/sig/client/client_install.rsp",
+    command     => "${installer_location}/client/runInstaller -silent -waitforcompletion -ignoreSysPrereqs -ignorePrereq -responseFile ${installer_location}/client_install.rsp",
     cwd         => $installer,
     require     => [File["client-response"],
                     File["oraInst"],
